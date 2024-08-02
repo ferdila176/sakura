f68c24d8e53c43decca4cff9030ddb44f93349b1
deviy <devi.yuningsih47@gmail.com>
2024-08-02 15:02:58 +0700

    feat: implementasi getelementbytagname

---
+++ DOM/main.js
@@ -0,0 +1,19 @@
+const header = document.getElementById('header');
+header.style.color = 'red';
+
+const element = document.getElementById("myDIV");
+element.classList.add("btn")
+element.classList.add("btn-primary")
+
+const elemen = document.getElementById("myDIV");
+element.classList.add("btn")
+element.classList.add("btn-primary")
+
+const paragraf = document.getElementsByTagName('p');
+// for(let i = 0; i <= paragraf.length; i++) {
+   paragraf[1].style.color = "orange"
+// }
+
+const Element = document.getElementById("addElementButton");
+Element.classList.add("btn")
+Element.classList.add("btn-outline-success")
